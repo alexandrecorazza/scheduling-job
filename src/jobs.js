b77a6256f93171f83c5cf4de9cba4762fa8e758a
Alexandre Corazza Marcolino <alexandre.corazza@ibm.com>
2023-08-28 13:07:14 -0300

    feat: add jobs input and window time

---
+++ src/jobs.js
@@ -0,0 +1,23 @@
+export const jobs = [
+  {
+    "ID": 1,
+    "Description": "Importing background files",
+    "Deadline": new Date("2019-11-10 12:00:00"),
+    "Estimated time": 2,
+  },
+  {
+    "ID": 2,
+    "Description": "Importing data from Legacy Database",
+    "Deadline": new Date("2019-11-11 12:00:00"),
+    "Estimated time": 4,
+  },
+  {
+    "ID": 3,
+    "Description": "Integration data import",
+    "Deadline": new Date("2019-11-11 08:00:00"),
+    "Estimated time": 6,
+  }
+]
+
+export const windowStartDate = new Date("2019-11-10 09:00:00")
+export const windowEndDate = new Date("2019-11-11 12:00:00")
